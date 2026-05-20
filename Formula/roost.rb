a79e347c6fb911ab31df63971f373adfbdaab5e1
phpgao <endoffight@gmail.com>
2026-05-20 08:17:21 +0000

    Update roost to v0.2.5

--- Formula/roost.rb
+++ Formula/roost.rb
@@ -1,23 +1,23 @@
 class Roost < Formula
   desc "Interactive AI Session manager TUI for Claude, Gemini, Codex, Copilot, OpenCode"
   homepage "https://github.com/phpgao/roost"
-  version "0.2.4"
+  version "0.2.5"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/phpgao/roost/releases/download/v#{version}/roost-darwin-arm64.tar.gz"
-      sha256 "82b19f75d76b3c9544aa083e3df97311a72bfabe9a14aa0f6ab009958ec6faf5"
+      sha256 "345a91faca7217bde20512880b44e66c3718a07e9c52e9b2abea58f3e1bc06bc"
     else
       url "https://github.com/phpgao/roost/releases/download/v#{version}/roost-darwin-amd64.tar.gz"
-      sha256 "6c484ef077ab0646a6dae394507095be44336f67c7f7441961490b6e8a9d73d4"
+      sha256 "d2f817896c735ab04b9638c1febf7afb6e7cade07a699ad9f0e59457d038118f"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/phpgao/roost/releases/download/v#{version}/roost-linux-arm64.tar.gz"
-      sha256 "4240a47cef5cc47a01e7b09b89a7692140243f2b8169980d25b3e6fa649f4891"
+      sha256 "ee7f3b12844f3763a6e939f9f72fee0a29edec5e5187e0ab5169dbee56bdba59"
     else
       url "https://github.com/phpgao/roost/releases/download/v#{version}/roost-linux-amd64.tar.gz"
-      sha256 "f51de301a9f196b225c971ba7659040e2420d23f3f40e5316ffdff1a3c1e858d"
+      sha256 "aec4541ad7c425fc91401b0205e39d6d7f1d964a7a48fd689d94ae9e31447b5d"
     end
   end
 
